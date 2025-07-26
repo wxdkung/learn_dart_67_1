7443395885620a9963828b85b63f26818060a653
wudkung <phuriwatphanchalad@gmail.com>
2025-07-26 14:59:19 +0700

    Step 3 Statice

---
+++ dart3.dart
@@ -0,0 +1,26 @@
+// Static Variable Type
+void main() {
+  // Sentitive Case
+  String name = "Alice";
+  String _name = "Bob";
+  String nAme = "Charlie";
+
+  print(name);
+  print(_name);
+  print(nAme);
+
+  print("----------------");
+
+  name = "David";
+  int age = 20;
+  double score = 95.5;
+  num height = 175.5, weight = 70;
+  bool isStudent = true;
+
+  print("ชื่อ: $name");
+  print("อายุ: $age");
+  print("คะแนน: $score");
+  print("ส่วนสูง: $height");
+  print("น้ำหนัก: $weight");
+  print("นักเรียน: $isStudent");
+}
