4f79ae6c5ba2d39c1f8ff8c829611d79352971c8
wudkung <phuriwatphanchalad@gmail.com>
2025-07-26 14:46:43 +0700

    Step 2

---
+++ dart2.dart
@@ -0,0 +1,11 @@
+// Import file
+import 'dart2a.dart';
+
+void main() {
+  // Accessing the public key
+  print("Public Key: $publicKey");
+
+  // Attempting to access the private key will result in an error
+  // Uncomenting the next line will cause a compilation error
+  // print("Secret Key: $_secretKey");
+}
---
+++ dart2a.dart
@@ -0,0 +1,2 @@
+String _secreKey = "12345";
+String publicKey = "6789";
