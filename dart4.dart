ae594d5993a6abeda920a73e489e142758e5c130
wudkung <phuriwatphanchalad@gmail.com>
2025-07-26 15:07:34 +0700

    Step 4 Dynamic Variale

---
+++ dart4.dart
@@ -0,0 +1,11 @@
+// Dynamic Variable Type
+void main() {
+  dynamic dynamicVariable = 42;
+  print(dynamicVariable);
+
+  dynamicVariable = "Hello, Dart!";
+  print(dynamicVariable);
+
+  dynamicVariable = [1, 2, 2];
+  print(dynamicVariable);
+}
