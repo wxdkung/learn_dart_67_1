36decf396272b3df6886d8570bd186a2b1c87ceb
wudkung <phuriwatphanchalad@gmail.com>
2025-07-26 14:10:58 +0700

    first commit

---
+++ dart1.dart
@@ -0,0 +1,23 @@
+void main() {
+  var name = "Jonh";
+  print(name);
+
+  print("Your name is $name.");
+  print("your name is ${name = 'Jane'}.");
+
+  int age = 20;
+  print("yourr age is $age.");
+
+  //Good variable names
+  String userNameColor = "Alice";
+  int _userAge = 12;
+  double temperatureCelsius = 36.6;
+  bool isActive = true;
+
+  // Bad variable names
+  // bool isInactive = true; // bool isActive = False;
+  // String 1name = "Bob";
+  // int max count = 50;
+  // double total% = 100.0; // totalPercent
+  //  var if = "Error"; // reseved keyword
+}
